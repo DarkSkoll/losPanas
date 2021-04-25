33620735e652d47b539ff044d68ccf709e1abb31
darkskoll <erick.lievana@gmail.com>
2021-04-25 11:20:31 -0500

    Add update system

---
+++ model/updateDB.php
@@ -0,0 +1,27 @@
+<?php
+include "dataBaseConnection.php";
+
+$nombreOri = $_GET['nombreOri'];
+$nombre = $_GET['nombre'];
+$descripcion = $_GET['descripcion'];
+$precio = $_GET['precio'];
+
+if (isset($_GET['actualizar'])) {
+  $sql = "UPDATE `menu` SET `nombre`='$nombre',`descripcion`='$descripcion',`precio`='$precio' WHERE `nombre`='$nombreOri'";
+  if(mysqli_query($conn, $sql)){
+    echo "Cambio en la base de datos";
+  } else {
+    echo "ERROR: no se pudo ejecutar $sql. " . mysqli_error($conn);
+  }
+  header("Location: /losPanas/view/menu.php");
+}
+elseif (isset($_GET['borrar'])) {
+  $sql = "DELETE FROM `menu` WHERE `nombre`='$nombreOri'";
+  if(mysqli_query($conn, $sql)){
+    echo "Cambio en la base de datos";
+  } else {
+    echo "ERROR: no se pudo ejecutar $sql. " . mysqli_error($conn);
+  }
+  header("Location: /losPanas/view/menu.php");
+}
+?>
--- view/editor.php
+++ view/editor.php
@@ -3,29 +3,22 @@ include "banner.php";
 include "../model/dataBaseConnection.php";
 include "../model/getMenuData.php";
 $nombreOri = $_GET['nombrePlatillo'];
-$results = getMenuItem($conn,$nombre);
+$results = getMenuItem($conn,$nombreOri);
+$descripcionOri = null;
+$precioOri = null;
 foreach ($results as $r) {
   $descripcionOri = $r['descripcion'];
   $precioOri = $r['precio'];
 }
 ?>
-<form method="get" accept-charset="utf-8">
+<form method="get" accept-charset="utf-8" action="../model/updateDB.php">
   <label for="nombre">Nombre :</label><br></br>
   <input type="text" name="nombre" value="<?php echo $nombreOri; ?>" size="80"><br></br>
   <label for="descripcion">Descripción :</label><br></br>
   <input type="text" name="descripcion" value="<?php echo $descripcionOri; ?>" size="80"></textarea><br></br>
-<label for="nombre">Precio :</label><br></br>
-<input type="text" name="nombre" value="<?php printf("%.2F",$precioOri); ?>" size="20"><br></br>
-<input type="submit" name="actualizar" value="Actualizar">
-<input type="submit" name="borrar" value="Eliminar">
+  <label for="precio">Precio :</label><br></br>
+  <input type="text" name="precio" value="<?php printf("%.2F",$precioOri); ?>" size="20"><br></br>
+  <input type="hidden" name="nombreOri" value="<?php echo $nombreOri; ?>">
+  <input type="submit" name="actualizar" value="Actualizar">
+  <input type="submit" name="borrar" value="Eliminar">
 </form>
-<?php
-if (isset($_GET['actualizar'])) {
-  $sql = "UPDATE menu SET nombre=$nombre, descripcion=$descripcion, precio=$precio WHERE nombre=$nombreOri";
-  mysqli_query($conn,$sql);
-  header("Location: /losPanas/view/menu.php");
-}
-elseif (isset($_GET['borrar'])) {
-  header("Location: /losPanas/view/menu.php");
-}
-?>
